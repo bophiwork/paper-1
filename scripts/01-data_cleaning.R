d063be32b17d8502d2373540b6c58aa00c86548e
Bo Phi <bo.phi@mail.utoronto.ca>
2022-02-06 13:31:27 +0000

    1st big push

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,8 +1,8 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Clean the survey data downloaded from Open Data Toronto
+# Author: Bo Phi
+# Data: 1/31/2022
+# Contact: bo.phi@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
 # - Need to have downloaded the ACS data and saved it to inputs/data
@@ -15,22 +15,18 @@
 # Use R Projects, not setwd().
 library(haven)
 library(tidyverse)
+library(opendatatoronto)
 # Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
+raw_data <- readr::read_csv("inputs/data/COVID19 cases.csv"
                      )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
-         
+#### Data cleaning ####
+reduced_data <- raw_data[-c(1,3,5,6,8,10,11,12,13,14,15,16,17,18)]
 
-#### What's next? ####
+#### Saving the reduced data ####
+write_csv(reduced_data,file = "inputs/data/reduced_data.csv")
 
-
+reduced_data <- readr::read_csv("inputs/data/reduced_data.csv"
+                      )
 
          
